942bb8c6295b09a34636d571284d030f94f4589b
Logon Aluno <logonaluno@local.com>
2019-09-21 11:58:35 -0300

    Exercício

--- Aula4.R
+++ Aula4.R
@@ -38,3 +38,16 @@ prev <- predict(mod, newdata=teste)
 cbind(prev, teste$PIB, prev - teste$PIB)
 sse<-sum((prev - teste$PIB)^2)
 
+
+#Auto Regressivo AR
+mod <- lm(PIB~PIBi1+PIBi2+PIBi12,data=treino)
+summary(mod)
+prev <- predict(mod, newdata=teste)
+cbind(prev, teste$PIB, prev - teste$PIB)
+sse<-sum((prev - teste$PIB)^2)
+
+#FSBDS23 Rodolfo
+#https://www.kaggle.com/c/titanic
+#Chance de sobrevivência do disastre, exercício para essa semana
+#Survived variável base tranin
+#Na base teste não existe a tabela survied
