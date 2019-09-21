7786ac0e8b4600b5d92f80d3b7fc2223b30b51b4
Logon Aluno <logonaluno@local.com>
2019-09-21 11:40:33 -0300

    RLM

--- Aula4.R
+++ Aula4.R
@@ -1,7 +1,33 @@
 # Aula 4
+
+# Carrega o dataset
 p <- read.csv("PIB.csv", sep=";", dec=",")
 
-#
-# 132 dez/14
+# Separa o dataset
+# até dez/14 linha 132
 treino <- p[1:132,]
 teste <- p[133:138,]
+
+#Regressão linear simples
+mod <- lm(PIB~BRL,data=treino)
+
+#Visualiza as informações
+summary(mod)
+
+#precisão do pib
+prev <- predict(mod, newdata=teste)
+
+#comparar bases
+cbind(prev, teste$PIB, prev - teste$PIB)
+
+#calcular a média do erro quadrado
+sse<-sum((prev - teste$PIB)^2)
+
+
+#Regressão linear Múltipla
+mod <- lm(PIB~BRL+BRP,data=treino)
+summary(mod)
+prev <- predict(mod, newdata=teste)
+cbind(prev, teste$PIB, prev - teste$PIB)
+sse<-sum((prev - teste$PIB)^2)
+
